f39426464b8c90a68c9c2ca3a5e6b30a4c7fca78
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-13 21:52:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
   
 def now_serving(current_line)
   if current_line.length == 0 
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
   next_person = current_line[0]
   current_line.shift
