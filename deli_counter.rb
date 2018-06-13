eea1122becf7a7ca1f6e85929b2f4cd624e4bccb
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-13 21:34:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(people_in_line)
 end
 
 def take_a_number(current_line, new_person) #"Welcome, Grace. You are number 4 in line."
-  
+  current_line.push(new_person)
   puts "Welcome, #{new_person}. You are number #{current_line.length} in line."
 end
   
